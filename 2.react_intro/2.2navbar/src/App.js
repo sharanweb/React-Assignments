3a9799d6e78dd6563be88d72413b83c6d9a1f1bb
Sharan Dooganavar <91963829+sharanweb@users.noreply.github.com>
2022-04-10 12:10:13 +0530

    updated navbar

--- 2.react_intro/2.2navbar/src/App.js
+++ 2.react_intro/2.2navbar/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const links = ['Services','Projects','About'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Sharan</h1>
+      </div>
+      <div className="flex">
+        {links.map((e)=>{
+          return <p>{e}</p>
+        })}
+      </div>
+      <div>
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
+
+// function Links(props) {
+// 	return <p>{props.link}</p>;
+// }
 
 export default App;
