22ab94670bedfa431c9634b2a23559481d90def3
Sharan Dooganavar <91963829+sharanweb@users.noreply.github.com>
2022-04-21 18:38:11 +0530

    updated functions

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {Timer} from "./Components/Timer.jsx"
 
 function App() {
   return (
     <div className="App">
+      <Timer></Timer>
       
     </div>
   );
